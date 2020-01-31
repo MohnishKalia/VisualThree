07ebe902f52d0d7de1c67b81208cd7b55da89880
MohnishKalia <mohnish.kalia@gmail.com>
2020-01-31 11:27:56 -0600

    Example setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Container extends React.Component {
+    state = { isMounted: true };
+
+    render() {
+        const { isMounted = true } = this.state;
+        return (
+            <>
+                <button
+                    onClick={() =>
+                        this.setState(state => ({ isMounted: !state.isMounted }))
+                    }
+                >
+                    {isMounted ? "Unmount" : "Mount"}
+                </button>
+                {isMounted && <App />}
+                {isMounted && <div>Scroll to zoom, drag to rotate</div>}
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<Container />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
